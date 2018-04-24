11048e9289508244e4c5bbd55ee1b7a3fd6e6aca
johans <duanqingbb@163.com>
2018-04-24 04:02:30 +0000

    --allow-empty-message

---
+++ Prepost/Data/Rule.php
@@ -0,0 +1,13 @@
+<?php
+namespace Pider\Prepost\Data;
+
+class Rule {
+    private $restrict;
+    public function __construct(Callable $restrict) {
+        $this->restrict = $restrict;
+    }
+    public function __invoke($data):bool {
+        $restrict_callback = $this->restrict;
+        return $restrict_callback($data); 
+    }
+}
